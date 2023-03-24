26f7a0e273916edc6038877361ff37780833a14e
Brendan Reidy <reidyb@uni.coventry.ac.uk>
2023-03-24 15:30:01 +0000

    Test script now automatically opens App

--- testing/test/specs/lockUnlockTest.js
+++ testing/test/specs/lockUnlockTest.js
@@ -1,11 +1,35 @@
 const assert = require('assert');
 
 
+describe('Specific element test', () => {
+    it('Expo Set-up - App opened in config, this inputs project URL, opens app, and closes pop up', async () => {
+        const selector = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup";
 
+        const el1 = await $(selector);
+        await el1.click();
+
+        const selector2 = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup[2]/android.widget.EditText";
+
+        const el2 = await $(selector2);
+        await el2.click();
+        await el2.setValue("exp://172.16.1.68:19000");
+
+        const selector3 = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup[2]/android.view.ViewGroup[2]/android.view.ViewGroup";
+
+        const el3 = await $(selector3);
+        await el3.click();
+
+        await browser.pause(5000);
+
+        const el4 = await driver.$("~Model");
+        await el4.click();
+
+    });
+});
 
 describe('Lock/Unlock button test', () => {
-    it('should lock and unlock the car based on the initial status', async () => {
-        await browser.pause(5000);
+    it('Toggles car lock/unlock twice and checks result', async () => {
+        await browser.pause(1000);
 
         const lockUnlockButton = await $('~lock-unlock-button');
         const lockUnlockStatus = await $('~lock-unlock-status');
@@ -24,6 +48,17 @@ describe('Lock/Unlock button test', () => {
             const newLockStatus = await lockUnlockStatus.getText();
             assert.strictEqual(newButtonStatus, 'Unlock');
             assert.strictEqual(newLockStatus, 'THE CAR IS LOCKED');
+
+            console.log('Second Test Starting in 5s');
+            await browser.pause(5000);
+
+            await lockUnlockButton.click();
+            await browser.pause(1000);
+            const newButtonStatusTest2 = await lockUnlockButtonStatus.getText();
+            const newLockStatusTest2 = await lockUnlockStatus.getText();
+            assert.strictEqual(newButtonStatusTest2, 'Lock');
+            assert.strictEqual(newLockStatusTest2, 'THE CAR IS UNLOCKED');
+
         } else if (initialButtonStatus === 'Unlock' && initialLockStatus === 'THE CAR IS LOCKED') {
             await lockUnlockButton.click();
             await browser.pause(1000);
@@ -31,8 +66,19 @@ describe('Lock/Unlock button test', () => {
             const newLockStatus = await lockUnlockStatus.getText();
             assert.strictEqual(newButtonStatus, 'Lock');
             assert.strictEqual(newLockStatus, 'THE CAR IS UNLOCKED');
+
+            console.log('Second Test Starting in 5s');
+            await browser.pause(5000);
+
+            await lockUnlockButton.click();
+            await browser.pause(1000);
+            const newButtonStatusTest2 = await lockUnlockButtonStatus.getText();
+            const newLockStatusTest2 = await lockUnlockStatus.getText();
+            assert.strictEqual(newButtonStatusTest2, 'Unlock');
+            assert.strictEqual(newLockStatusTest2, 'THE CAR IS LOCKED');
         } else {
             console.log('The initial status is inconsistent. Please check the app.');
         }
     });
 });
+
